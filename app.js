7f95508de0a6f06e059907438893d6b81f65d458
marceldantas <allefmarcel.dev@gmail.com>
2021-08-16 01:36:59 -0300

    responsive-form

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const container = document.querySelector(".container");
 sign_up_btn.addEventListener('click', () => {
     container.classList.add("sign-up-mode")
 });
+
+sign_in_btn.addEventListener('click', () => {
+    container.classList.remove("sign-up-mode")
+});
